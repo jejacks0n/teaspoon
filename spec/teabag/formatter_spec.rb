ba59cd1ec76762b48a6d069baa1c69a77436517a
jejacks0n <jejacks0n@gmail.com>
2012-12-11 00:57:25 -0700

    fixed a broken spec.

--- spec/teabag/formatter_spec.rb
+++ spec/teabag/formatter_spec.rb
@@ -82,11 +82,11 @@ describe Teabag::Formatter do
     describe "with failures" do
 
       it "logs the details and raises an exception" do
-        failures = {"spec" => "some spec", "description" => "some description", "link" => "?grep=some%20spec"}
+        failures = {"spec" => "some spec", "message" => "some message", "link" => "?grep=some%20spec"}
         expect {
           subject.results("elapsed" => 0.31337, "failures" => [failures], "pending" => [], "total" => 666)
         }.to raise_error(Teabag::Failure)
-        expect(@log).to eq("\n\nFailures:\n\n  1) some spec\n\e[31m     Failure/Error: some description\n\e[0m\nFinished in 0.31337 seconds\n\e[31m666 examples, 1 failure\n\e[0m\nFailed examples:\n\e[31m\n/teabag/default?grep=some%20spec\e[0m\n\n")
+        expect(@log).to eq("\n\nFailures:\n\n  1) some spec\n\e[31m     Failure/Error: some message\n\e[0m\nFinished in 0.31337 seconds\n\e[31m666 examples, 1 failure\n\e[0m\nFailed examples:\n\e[31m\n/teabag/default?grep=some%20spec\e[0m\n\n")
       end
 
     end
