ea7679f00ae6b2c84f3ae19056b751b3a35b1a74
jejacks0n <jejacks0n@gmail.com>
2012-12-13 19:40:46 -0700

    added AssetNotServable exception and rescue from clause in the controller so we can respond to exceptions with javascript.

--- app/controllers/teabag/spec_controller.rb
+++ app/controllers/teabag/spec_controller.rb
@@ -3,6 +3,10 @@ class Teabag::SpecController < ActionController::Base
 
   layout false
 
+  unless Rails.application.config.assets.debug
+    rescue_from Teabag::AssetNotServable, with: :javascript_exception
+  end
+
   def index
     @suite = Teabag::Suite.new(params[:suite])
   end
@@ -11,4 +15,12 @@ class Teabag::SpecController < ActionController::Base
     prepend_view_path Teabag.configuration.root.join(Teabag.configuration.fixture_path)
     render "/#{params[:filename]}"
   end
+
+  private
+
+  def javascript_exception(exception)
+    err  = "#{exception.class.name}: #{exception.message}"
+    render text: "<script>throw Error(#{err.inspect})</script>"
+  end
+
 end
--- lib/teabag/exceptions.rb
+++ lib/teabag/exceptions.rb
@@ -2,4 +2,5 @@ module Teabag
   class Failure < Exception; end
   class UnknownSuite < Exception; end
   class RunnerException < Exception; end
+  class AssetNotServable < Exception; end
 end
