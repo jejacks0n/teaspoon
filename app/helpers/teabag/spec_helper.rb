467adc0716d643cfbbb67410feb38d0467789bb4
jejacks0n <jejacks0n@gmail.com>
2013-01-01 19:18:00 -0700

    should work with rails 4
    - added shims for the asset helpers, but these will hopefully be addressed
    - pull request on rails (https://github.com/rails/rails/pull/8681)
    - pull request on sprockets-rails (https://github.com/rails/sprockets-rails/pull/33)
    - issue opened on sprockets-rails (https://github.com/rails/sprockets-rails/issues/32) -- though I suspect it's an issue in rails.
    - the shims that we've been adding can cause issues for people who prefix their assets differently.

--- app/helpers/teabag/spec_helper.rb
+++ app/helpers/teabag/spec_helper.rb
@@ -1,10 +1,19 @@
 module Teabag::SpecHelper
 
+  def stylesheet_link_tag_for_teabag(*sources)
+    sources.collect do |source|
+      asset = defined?(lookup_asset_for_path) ? lookup_asset_for_path(source, type: :stylesheet) : asset_paths.asset_for(source, "css")
+      asset.to_a.map do |dep|
+        stylesheet_link_tag(dep.pathname.to_s, href: "/assets/#{dep.logical_path}?body=1", type: "text/css")
+      end
+    end.flatten.uniq.join("\n").html_safe
+  end
+
   def javascript_include_tag_for_teabag(*sources)
     sources.collect do |source|
-      asset = asset_paths.asset_for(source, "js")
+      asset = defined?(lookup_asset_for_path) ? lookup_asset_for_path(source, type: :javascript) : asset_paths.asset_for(source, "js")
       asset.to_a.map do |dep|
-        javascript_include_tag(dep.pathname.to_s, src: path_to_asset(dep, ext: "js", body: true), debug: false)
+        javascript_include_tag(dep.pathname.to_s, src: "/assets/#{dep.logical_path}?body=1", type: "text/javascript")
       end
     end.flatten.uniq.join("\n").html_safe
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Teabag::Engine.routes.draw do
-  match "/fixtures/*filename", to: "spec#fixtures"
-  match "/(:suite)", :to => "spec#index", defaults: { suite: nil }
+  match "/fixtures/*filename", to: "spec#fixtures", via: :get
+  match "/(:suite)", to: "spec#index", via: :get, defaults: { suite: "default" }
 end
