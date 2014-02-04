cb4df5805857f5370bc33a112f4413c456b20ce7
jejacks0n <jejacks0n@gmail.com>
2014-02-03 18:09:08 -0700

    removes node stuff, since it should be in your path.. if it's not, add it.

--- lib/teaspoon/utility.rb
+++ lib/teaspoon/utility.rb
@@ -1,21 +1,20 @@
 module Teaspoon
   module Utility
 
+    # Cross-platform way of finding an executable in the $PATH.
+    # http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    #
     def which(cmd)
       exts = ENV["PATHEXT"] ? ENV["PATHEXT"].split(";") : [""]
 
       ENV["PATH"].split(File::PATH_SEPARATOR).each do |path|
         exts.each do |ext|
-          exe = "#{ path }/#{ cmd }#{ ext }"
+          exe = "#{path}/#{cmd}#{ext}"
           return exe if File.executable?(exe)
         end
       end
 
-      node_module?(cmd) || nil
-    end
-
-    def node_module?(cmd)
-      "./node_modules/.bin/#{cmd}" if File.executable?("./node_modules/.bin/#{cmd}")
+      nil
     end
   end
 end
