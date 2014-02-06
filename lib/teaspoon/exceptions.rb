97f12c71615eac63e2c1545c46a03d6a617e4258
jejacks0n <jejacks0n@gmail.com>
2014-02-06 00:07:37 -0700

    adds more exceptions, and restructures a bit.

--- lib/teaspoon/exceptions.rb
+++ lib/teaspoon/exceptions.rb
@@ -1,13 +1,17 @@
 module Teaspoon
-  class EnvironmentNotFound < Exception; end
-  class MissingDependency < Exception; end
-  class ServerException < Exception; end
-  class RunnerException < Exception; end
-  class UnknownDriver < Exception; end
-  class UnknownFormatter < Exception; end
-  class UnknownSuite < Exception; end
-  class AssetNotServable < Exception; end
-  class Failure < Exception; end
+  class Error < Exception; end
+  class EnvironmentNotFound < Teaspoon::Error; end
+  class MissingDependency < Teaspoon::Error; end
+  class ServerException < Teaspoon::Error; end
+  class RunnerException < Teaspoon::Error; end
+  class UnknownDriver < Teaspoon::Error; end
+  class UnknownFormatter < Teaspoon::Error; end
+  class UnknownSuite < Teaspoon::Error; end
+  class AssetNotServable < Teaspoon::Error; end
+  class Failure < Teaspoon::Error; end
+  class DependencyFailure < Teaspoon::Error; end
+  class ThresholdNotMet < Teaspoon::Error; end
+  class FileNotWritable < Teaspoon::Error; end
 
   module ExceptionHandling
 
