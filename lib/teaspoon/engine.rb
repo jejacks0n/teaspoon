268c00fa37e058a4bcf3e917f1a128a3bdb991c2
jejacks0n <jejacks0n@gmail.com>
2014-02-25 23:18:11 -0700

    adjusts engine to call class level methods (rails 4.1 issue)

--- lib/teaspoon/engine.rb
+++ lib/teaspoon/engine.rb
@@ -14,32 +14,32 @@ module Teaspoon
         # it's ok for this to fail sometimes, like before the initializer is run etc
       end
 
-      default_root_path(app.root)                 # default the root if it's not set
-      append_asset_paths(app.config.assets)       # append the asset paths from the configuration
+      Teaspoon::Engine.default_root_path(app.root)           # default the root if it's not set
+      Teaspoon::Engine.append_asset_paths(app.config.assets) # append the asset paths from the configuration
     end
 
     config.after_initialize do |app|
-      inject_instrumentation                      # inject our sprockets hack for instrumenting javascripts
-      prepend_routes(app)                         # prepend routes so a catchall doesn't get in the way
+      Teaspoon::Engine.inject_instrumentation                # inject our sprockets hack for instrumenting javascripts
+      Teaspoon::Engine.prepend_routes(app)                   # prepend routes so a catchall doesn't get in the way
     end
 
     private
 
-    def default_root_path(root)
+    def self.default_root_path(root)
       Teaspoon.configuration.root ||= root
     end
 
-    def append_asset_paths(assets)
+    def self.append_asset_paths(assets)
       Teaspoon.configuration.asset_paths.each do |path|
         assets.paths << Teaspoon.configuration.root.join(path).to_s
       end
     end
 
-    def inject_instrumentation
+    def self.inject_instrumentation
       Sprockets::Environment.send(:include, Teaspoon::SprocketsInstrumentation)
     end
 
-    def prepend_routes(app)
+    def self.prepend_routes(app)
       return if Teaspoon::Engine.routes.recognize_path('/') rescue nil
       require Teaspoon::Engine.root.join("app/controllers/teaspoon/suite_controller")
 
