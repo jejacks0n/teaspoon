bae86390959416879a5316c4dcb6d74c734b9289
Mark Tareshawty <tarebyte@gmail.com>
2014-05-20 09:48:00 -0400

    added slimerjs spec

---
+++ spec/teaspoon/drivers/slimerjs_driver_spec.rb
@@ -0,0 +1,61 @@
+require "spec_helper"
+
+describe Teaspoon::Drivers::SlimerjsDriver do
+
+  describe "#initialize" do
+
+    it "assigns @options" do
+      subject = Teaspoon::Drivers::SlimerjsDriver.new(foo: "bar")
+      expect(subject.instance_variable_get(:@options)).to eq(["--foo=bar"])
+    end
+
+    it "accepts a string for options" do
+      subject = Teaspoon::Drivers::SlimerjsDriver.new("--foo=bar --bar=baz")
+      expect(subject.instance_variable_get(:@options)).to eq(["--foo=bar", "--bar=baz"])
+    end
+
+    it "accepts an array for options" do
+      subject = Teaspoon::Drivers::SlimerjsDriver.new(["--foo=bar", "--bar=baz"])
+      expect(subject.instance_variable_get(:@options)).to eq(["--foo=bar", "--bar=baz"])
+    end
+
+    it "raises a Teaspoon::UnknownDriverOptions exception if the options aren't understood" do
+      expect { Teaspoon::Drivers::SlimerjsDriver.new(true) }.to raise_error(Teaspoon::UnknownDriverOptions)
+    end
+
+  end
+
+  describe "#run_specs" do
+
+    context "with slimerjs" do
+
+      let(:runner) { double }
+
+      before do
+        subject.stub(:run)
+      end
+
+      it "calls #run and calls runner.process with each line of output" do
+        subject.instance_variable_set(:@options, ["--foo", "--bar"])
+        args = ["--foo", "--bar", Teaspoon::Engine.root.join("lib/teaspoon/drivers/slimerjs/runner.js").to_s, "_url_", 180]
+        runner.should_receive(:process).with("_line_")
+        @block = nil
+        subject.should_receive(:run).with(*args) { |&b| @block = b }
+        subject.run_specs(runner, "_url_")
+        @block.call("_line_")
+      end
+
+    end
+
+    context "without slimerjs" do
+
+      it "raises a MissingDependency exception" do
+        subject.should_receive(:which).and_return(nil)
+        expect { subject.run_specs(:default, "_url_") }.to raise_error Teaspoon::MissingDependency
+      end
+
+    end
+
+  end
+
+end
