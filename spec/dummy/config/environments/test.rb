37571a98c773dc825eb12737cb58ba7578e3c696
jejacks0n <jejacks0n@gmail.com>
2015-02-22 12:11:29 -0700

    fixes deprecation warning.

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -8,18 +8,18 @@ Dummy::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
+  config.action_controller.allow_forgery_protection = false
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
