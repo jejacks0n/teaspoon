1a5582c06a89702ac26678b6d0688a4b8a9ef9ae
jejacks0n <jejacks0n@gmail.com>
2015-03-15 22:47:07 -0600

    provides jasmine(1 and 2) specific test suite.

--- teaspoon-jasmine/spec/javascripts/jasmine2_helper.coffee
+++ teaspoon-jasmine/spec/javascripts/jasmine2_helper.coffee
@@ -1,3 +1,2 @@
 #= require jquery
 #= require support/jasmine-jquery-2.1.0
-#= require spec_helper
--- teaspoon-jasmine/spec/teaspoon_env.rb
+++ teaspoon-jasmine/spec/teaspoon_env.rb
@@ -1,18 +1,19 @@
+require "teaspoon-devkit"
+Teaspoon.require_dummy!
+
 Teaspoon.configure do |config|
-  path = "teaspoon-jasmine/spec/javascripts"
+  config.asset_paths << path = File.expand_path("../javascripts", __FILE__)
+  config.fixture_paths << Teaspoon::FIXTURE_PATH
 
-  config.asset_paths << Teaspoon::Engine.root.join(path)
-
-  config.suite :jasmine1 do |suite|
+  config.suite do |suite|
     suite.use_framework :jasmine, "1.3.1"
     suite.matcher = "#{path}/jasmine1/**/*_spec.{js,js.coffee,coffee}"
     suite.helper = "jasmine1_helper"
   end
 
   config.suite :jasmine2 do |suite|
-    suite.use_framework :jasmine, "2.2.0"
+    suite.use_framework :jasmine
     suite.matcher = "#{path}/jasmine2/**/*_spec.{js,js.coffee,coffee}"
     suite.helper = "jasmine2_helper"
   end
-
 end
