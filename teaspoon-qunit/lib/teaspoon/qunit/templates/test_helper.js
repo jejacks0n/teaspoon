c8bbd53b6aba34b0cc768f0b5cb8acbdbc45347e
Mike Pack <mikepackdev@gmail.com>
2015-05-05 16:36:33 -0600

    Use phantomjs-shims for polyfill
    Fixes #360

--- teaspoon-qunit/lib/teaspoon/qunit/templates/test_helper.js
+++ teaspoon-qunit/lib/teaspoon/qunit/templates/test_helper.js
@@ -4,7 +4,7 @@
 //
 // PhantomJS (Teaspoons default driver) doesn't have support for Function.prototype.bind, which has caused confusion.
 // Use this polyfill to avoid the confusion.
-//= require support/bind-poly
+//= require support/phantomjs-shims
 //
 // You can require your own javascript files here. By default this will include everything in application, however you
 // may get better load performance if you require the specific files that are being used in the test that tests them.
