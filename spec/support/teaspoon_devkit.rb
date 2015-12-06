cdcdc7d5d1608d6eaa9fc57e1b6e17552346fb1d
Mike Pack <mikepackdev@gmail.com>
2015-12-06 14:42:59 -0700

    Upgrade to aruby 0.11.x

--- spec/support/teaspoon_devkit.rb
+++ spec/support/teaspoon_devkit.rb
@@ -39,12 +39,12 @@ module Teaspoon
 
     def run_teaspoon(opts = "")
       set_rails_env
-      run_simple("bundle exec teaspoon #{opts}", false)
+      run_simple("bundle exec teaspoon #{opts}", fail_on_error: false)
     end
 
     def rake_teaspoon(envs = "")
       set_rails_env
-      run_simple("bundle exec rake teaspoon #{envs}", false)
+      run_simple("bundle exec rake teaspoon #{envs}", fail_on_error: false)
     end
 
     def copy_integration_files(suffix, from, to = "spec")
