bdbbeb03619f613b85a6ee022dc80d797a5fdbb0
jejacks0n <jejacks0n@gmail.com>
2016-08-18 21:56:09 -0600

    gets tests passing.

---
+++ spec/dummy/app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link teaspoon/jasmine1.js
+//= link_directory ../../../../../teaspoon-jasmine/spec/javascripts .js
+//= link_directory ../../../../../teaspoon-mocha/spec/javascripts .js
+//= link_directory ../../../../../teaspoon-qunit/test/javascripts/integration .js
