cd8101ed03a25c2be132d5ae0d28ada840c49b8d
Mathieu Jobin <mathieu@justbudget.com>
2021-03-12 07:25:34 +0900

    commenting out deprecated desired capabilities that is now erroring out

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,10 +2,10 @@ require "webdrivers"
 
 def register_driver(name, args = [], opts = {})
   Capybara.register_driver(name) do |app|
-    options = { args: args + ["window-size=1440,1080"] }
-    options[:binary] = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: options.compact)
-    Capybara::Selenium::Driver.new(app, { browser: :chrome, desired_capabilities: capabilities }.merge(opts))
+    #options = { args: args + ["window-size=1440,1080"] }
+    #options[:binary] = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
+    #capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: options.compact)
+    Capybara::Selenium::Driver.new(app, { browser: :chrome }.merge(opts))
   end
 end
 
