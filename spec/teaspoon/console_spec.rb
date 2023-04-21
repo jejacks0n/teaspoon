8f062d31880a1f81000d8a3bd9ee27ebb9f43712
Max Hoffmann <max.hoffmann@shopify.com>
2023-04-20 17:08:01 -0700

    Adding support for multiple suites through options (#511)
    * Adding support for multiple suites through options
    
    * flip order
    
    ---------
    
    Co-authored-by: Mathieu Jobin <99191+mathieujobin@users.noreply.github.com>

--- spec/teaspoon/console_spec.rb
+++ spec/teaspoon/console_spec.rb
@@ -120,6 +120,14 @@ describe Teaspoon::Console do
       expect(subject.send(:filter, "foo")).to eq("file[]=file2")
     end
 
+    it 'resolves suites if multiple are given' do
+      subject.execute_without_handling(suite: 'bar,foo')
+      expect(subject.send(:suites)).to eq(%w(bar foo))
+
+      subject.execute_without_handling(suite: 'foo')
+      expect(subject.send(:suites)).to eq(%w(foo))
+    end
+
     it "resolves the files if a directory was given" do
       resolve_spec_for_output = ["test/javascripts/foo.coffee", "test/javascripts/bar.coffee"]
       expect(Teaspoon::Suite).to receive(:resolve_spec_for).with("full/path").and_return(
